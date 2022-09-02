37ac1c94403ffc63034535d7f9466a59e83129c7
PetterFogel <petterfogel96@gmail.com>
2022-09-02 16:02:16 +0200

    Implement logic for routing + volvos theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
 import { AppProps } from "next/app";
+import { Logo, StyleProvider, TabNav, ThemePicker, View } from "vcc-ui";
 import React from "react";
 import "../public/css/styles.css";
+import Link from "next/link";
 
 function HomePage({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
-      <Component {...pageProps} />
+      <StyleProvider>
+        <ThemePicker variant="light">
+          <TabNav textAlign="left">
+            <View padding={3}>
+              <Link href={"/"}>
+                <Logo type="spreadmark" height={8} />
+              </Link>
+            </View>
+          </TabNav>
+          <Component {...pageProps} />
+        </ThemePicker>
+      </StyleProvider>
     </React.StrictMode>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
 import { NextPage } from "next";
+import { Button, Link, Logo, Spacer, Text, View } from "vcc-ui";
 
 const HomePage: NextPage = () => {
   return (
-    <div>
-      <h1>Welcome To Volvo</h1>
-    </div>
+    <View
+      spacing={3}
+      maxWidth={1000}
+      margin="auto"
+      display="flex"
+      justifyContent={"center"}
+      alignItems="center"
+      height={"70vh"}
+    >
+      <View padding={5}>
+        <Logo type="spreadmark" height={32} />
+      </View>
+      <View padding={3}>
+        <Text variant={"bates"} as="h2">
+          Checkout our recharge cars
+        </Text>
+      </View>
+      <View>
+        <Link href={"/recharge-cars"}>
+          <Button>Shop</Button>
+        </Link>
+      </View>
+    </View>
   );
 };
 
