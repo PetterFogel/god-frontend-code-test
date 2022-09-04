0bac0565970373872d0d81e598744999fd0c0c9b
PetterFogel <petterfogel96@gmail.com>
2022-09-04 19:25:46 +0200

    Chagned settings

--- src/components/common/constants/sliderSettings.ts
+++ src/components/common/constants/sliderSettings.ts
@@ -19,6 +19,7 @@ export const sliderSettings: Settings = {
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        dots: true,
       },
     },
     {
@@ -26,6 +27,7 @@ export const sliderSettings: Settings = {
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        dots: true,
       },
     },
   ],
