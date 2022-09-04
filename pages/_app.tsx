94052b5fff54de232aaa78cb606fec2de9646c68
PetterFogel <petterfogel96@gmail.com>
2022-09-04 19:22:18 +0200

    Implement header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,14 @@ import { Logo, StyleProvider, TabNav, ThemePicker, View } from "vcc-ui";
 import React from "react";
 import "../public/css/styles.css";
 import Link from "next/link";
+import { Header } from "../src/components/common/components/header/Header";
 
 function HomePage({ Component, pageProps }: AppProps) {
   return (
     <React.StrictMode>
       <StyleProvider>
         <ThemePicker variant="light">
-          <TabNav textAlign="left">
-            <View padding={3}>
-              <Link href={"/"}>
-                <Logo type="spreadmark" height={8} />
-              </Link>
-            </View>
-          </TabNav>
+          <Header />
           <Component {...pageProps} />
         </ThemePicker>
       </StyleProvider>
